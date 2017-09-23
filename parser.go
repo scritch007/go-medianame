3df9c976fe4ea59196a600ab36cd5e52ea204a23
Benjamin Legrand <dev@legrand.ws>
2017-09-23 21:29:39 +0200

    Cleanup name. Now we can begin to dig in it

--- parser.go
+++ parser.go
@@ -3,7 +3,9 @@ package moviename
 import (
 	"fmt"
 	"log"
+	"path/filepath"
 	"regexp"
+	"strings"
 )
 
 //Movie stores the movie Information
@@ -26,12 +28,34 @@ func NewMovieParser() MovieParser {
 
 //Parse parse name and return Movie or error
 func (m *MovieParser) Parse(name string) (Movie, error) {
+	//Remove extension
+	ext := filepath.Ext(name)
+	name = name[:len(name)-len(ext)]
+	fmt.Printf("Extension %s %s\n", ext, name)
 	//Move [ ] to the end
-	re := regexp.MustCompile("^\\[(?P<info>.*)\\](?P<name>.*)")
+	re := regexp.MustCompile("^\\[(?P<info>[^\\]]*)\\](?P<name>.*)")
 	if re.MatchString(name) {
-		reversed := fmt.Sprintf("${%s} ${%s}", re.SubexpNames()[2], re.SubexpNames()[1])
+		tmp := re.FindStringSubmatch(name)
 
-		name = re.ReplaceAllString(name, reversed)
+		name = fmt.Sprintf("%s %s", tmp[2], tmp[1])
 	}
+
+	//Replace special characters
+	for _, c := range "[]()_,." {
+		name = strings.Replace(name, string(c), " ", -1)
+	}
+
+	if strings.Index(name, " ") == -1 {
+		name = strings.Replace(name, "-", " ", -1)
+	}
+
+	for _, c := range []string{"imax"} {
+		name = strings.Replace(name, c, "", -1)
+	}
+
+	//Remove duplicated spaces
+	spaceRe := regexp.MustCompile("  +")
+	name = string(spaceRe.ReplaceAll([]byte(strings.TrimSpace(name)), []byte(" ")))
+
 	return Movie{Name: name}, nil
 }
